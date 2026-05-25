8ad534ad46c03b04fa63d288d3183e9375b4b16d
KEV-AJ <porcher3842@gmail.com>
2026-05-25 16:23:01 +0200

    add contenu page artisans

--- frontend/src/pages/detailsArtisan.js
+++ frontend/src/pages/detailsArtisan.js
@@ -1,7 +1,61 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 function Details() {
-    return(
-        <h1>Details</h1>
-    );
+
+  const { id } = useParams();
+
+  const [artisan, setArtisan] = useState(null);
+
+  useEffect(() => {
+
+    fetch(`http://localhost:3080/api/artisans/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setArtisan(data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+
+  }, [id]);
+
+  if (!artisan) {
+    return <h1>Chargement...</h1>;
+  }
+
+  return (
+
+    <div className="container mt-5">
+
+      <div className="card p-4 shadow">
+
+        <h1>{artisan.Nom}</h1>
+
+        <p>
+          <strong>Ville :</strong> {artisan.Ville}
+        </p>
+
+        <p>
+          <strong>Note :</strong> {artisan.Note}
+        </p>
+
+        <p>
+  <strong>Spécialité :</strong>{" "}
+  {artisan.Specialite?.Nom}
+</p>
+
+<p>
+  <strong>Catégorie :</strong>{" "}
+  {artisan.Specialite?.Categorie?.Nom}
+</p>
+
+      </div>
+
+    </div>
+
+  );
 }
 
 export default Details;
