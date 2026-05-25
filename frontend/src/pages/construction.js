380830d0ce7ac70fcf53c7c498271a6b1d6f6d90
KEV-AJ <porcher3842@gmail.com>
2026-05-25 13:55:03 +0200

    add contenue page erreur 404 et en construction

--- frontend/src/pages/construction.js
+++ frontend/src/pages/construction.js
@@ -1,6 +1,13 @@
 function Construction() {
     return(
-        <h1>en construction</h1>
+        <main>
+        <div>
+        <h2>Page en attente de construction</h2>
+        </div>
+        <div>
+            <img src='/images/icon-en-construction.png' alt="icon page en cours de construction"/>
+        </div>
+        </main>
     );
 }
 
